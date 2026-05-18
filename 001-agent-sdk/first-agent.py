d2e9d128a9caa6d895673efc4b4f65ddb6584536
Nils Diekmann <github@kinneko.de>
2026-05-18 15:20:11 +0200

    - extend agent example by model - add comment regarding authentication

--- 001-agent-sdk/first-agent.py
+++ 001-agent-sdk/first-agent.py
@@ -1,5 +1,6 @@
 # Using the agent sdk
 # Using the credential from the claude code plugin, no api key needed
+# I run this from vs code. If it does not work, then use the chat interface to login again
 # Is a task
 # https://code.claude.com/docs/en/agent-sdk/overview#python
 # pip install claude-agent-sdk
@@ -10,7 +11,10 @@ from claude_agent_sdk import query, ClaudeAgentOptions
 async def main():
     async for message in query(
         prompt="What files are in this directory?",
-        options=ClaudeAgentOptions(allowed_tools=["Bash", "Glob"]),
+        options=ClaudeAgentOptions(
+            # model="claude-haiku-4-5",
+            allowed_tools=["Bash", "Glob"]
+        ),
     ):
         if hasattr(message, "result"):
             print(message.result)
